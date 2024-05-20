3418f9e152437c0ac957101e105a424840d33022
RohanMenwani <menwanirohanwork@gmail.com>
2024-05-20 21:24:58 +0530

    Initial commit

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favourite food here</h2>
+                <p>Choose from a diverse </p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.tsx
@@ -0,0 +1,9 @@
+import "./PlaceHolder.css"
+
+const PlaceOrder = () => {
+    return (
+        <div>PlaceOrder</div>
+    )
+}
+
+export default PlaceOrder
